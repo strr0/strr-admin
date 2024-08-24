827a27b07e2833d73df7db77b4105d8762a2bdf2
strr0 <38580170+strr0@users.noreply.github.com>
2024-08-24 21:46:08 +0800

    feat: 系统模块

---
+++ strr-module/strr-system/src/main/java/com/strr/system/dubbo/RemoteUserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.strr.system.dubbo;
+
+import com.strr.system.api.RemoteUserService;
+import com.strr.system.api.model.LoginUser;
+import com.strr.system.mapper.SysResourceMapper;
+import com.strr.system.mapper.SysUserMapper;
+import com.strr.system.model.SysResource;
+import com.strr.system.model.SysUser;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * 用户服务
+ */
+@Service
+@DubboService
+public class RemoteUserServiceImpl implements RemoteUserService {
+    private final SysUserMapper sysUserMapper;
+    private final SysResourceMapper sysResourceMapper;
+
+    public RemoteUserServiceImpl(SysUserMapper sysUserMapper, SysResourceMapper sysResourceMapper) {
+        this.sysUserMapper = sysUserMapper;
+        this.sysResourceMapper = sysResourceMapper;
+    }
+
+    /**
+     * 获取用户信息
+     */
+    @Override
+    public LoginUser getUserInfo(String username) {
+        SysUser user = sysUserMapper.getByUsername(username);
+        if (user != null) {
+            LoginUser loginUser = new LoginUser();
+            loginUser.setId(user.getId());
+            loginUser.setUsername(user.getUsername());
+            loginUser.setPassword(user.getPassword());
+            loginUser.setNickname(user.getNickname());
+            loginUser.setResources(sysResourceMapper.listByUserId(user.getId())
+                    .stream().map(SysResource::getPerms).filter(Objects::nonNull)
+                    .collect(Collectors.toList()));
+            return loginUser;
+        }
+        return null;
+    }
+}
--- strr-module/strr-system/src/main/java/com/strr/system/mapper/SysUserMapper.java
+++ strr-module/strr-system/src/main/java/com/strr/system/mapper/SysUserMapper.java
@@ -4,4 +4,8 @@ import com.strr.system.model.SysUser;
 import com.strr.base.mapper.CrudMapper;
 
 public interface SysUserMapper extends CrudMapper<SysUser, Integer> {
+    /**
+     * 获取用户信息
+     */
+    SysUser getByUsername(String username);
 }
