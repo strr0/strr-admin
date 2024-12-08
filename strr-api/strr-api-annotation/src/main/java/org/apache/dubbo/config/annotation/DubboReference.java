0a391dab8a19dffaa6ca1fd71dfeabe5a56d7893
strr0 <38580170+strr0@users.noreply.github.com>
2024-12-09 00:14:17 +0800

    feat: dubbo 注解

---
+++ strr-api/strr-api-annotation/src/main/java/org/apache/dubbo/config/annotation/DubboReference.java
@@ -0,0 +1,14 @@
+package org.apache.dubbo.config.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Autowired
+public @interface DubboReference {
+}
